f43e73655ddb96035870cda0ba34aeb4263b806c
yy <yjiao7@uci.edu>
2020-08-09 08:58:35 +0800

    change

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,5 +1,6 @@
 export default {
-  navigationBarTitleText: "调查问卷"
+  navigationBarTitleText: "调查问卷",
+  navigationBarBackgroundColor: "#27b67a"
   // usingComponents: {
   //   "nav-bar": "./../navbar/navbar" // 书写第三方组件的相对路径
   // }
